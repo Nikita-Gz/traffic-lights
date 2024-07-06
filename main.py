0c49d2f73127cda837bb7b5a1c48b4a435b73ca6
Nikita-Gz <ngez.work@gmail.com>
2024-07-06 23:06:37 +0300

    new reward based on passed vehicles

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from simulation import TrafficIntersection
-from rewards import reward_based_on_avg_wait_time
+from rewards import reward_based_on_passed_vehicles
 from evaluation import evaluate_agent
 from agents import TimeBasedAgent
 from plotting import plot_evaluation_stats, plot_car_count_on_directions
 
 
 if __name__ == "__main__":
-    env = TrafficIntersection()
-    agent = TimeBasedAgent()
+    env = TrafficIntersection(
+        arrival_prob=0.2,
+    )
+    agent = TimeBasedAgent(light_duration=60)
     result = evaluate_agent(
         env=env,
         agent=agent,
-        reward_function=reward_based_on_avg_wait_time,
+        reward_function=reward_based_on_passed_vehicles,
         num_steps=1000,
     )
     plot_evaluation_stats(result)
--- rewards.py
+++ rewards.py
@@ -5,6 +5,10 @@ def reward_based_on_vehicles_in_queue(step_result: StepResult) -> float:
     return -sum(len(wait_times) for wait_times in step_result.wait_times_in_directions)
 
 
+def reward_based_on_passed_vehicles(step_result: StepResult) -> float:
+    return step_result.passed_vehicles
+
+
 def reward_based_on_max_wait_time(step_result: StepResult) -> float:
     max_per_direction = [
         max(wait_times) if wait_times else 0
